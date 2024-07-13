76a84630bd62d90e465fdff8b17649255a0a43f2
Bruno Silvano Costa <brunosilvanocosta@outlook.com>
2024-07-13 15:10:41 +0200

    Rename osc -> oscillator in Synth class

--- src/Synth/index.ts
+++ src/Synth/index.ts
@@ -4,10 +4,10 @@ class Synth {
   audioCtx: AudioContext;
 
   envelope: Envelope;
-  osc: OscillatorNode;
+  oscillator: OscillatorNode;
 
   setNote (note: number) {
-    this.osc.frequency.setValueAtTime(Math.pow(2, (note - 69) / 12) * 440, this.audioCtx.currentTime);
+    this.oscillator.frequency.setValueAtTime(Math.pow(2, (note - 69) / 12) * 440, this.audioCtx.currentTime);
   }
 
   constructor() {
@@ -18,10 +18,10 @@ class Synth {
     this.envelope.connect(this.audioCtx.destination);
 
     // create an osc to get some sound working
-    this.osc = this.audioCtx.createOscillator();
-    this.osc.frequency.setValueAtTime(440, this.audioCtx.currentTime); // set osc frequency to 440 Hz
-    this.osc.connect(this.envelope.destination());
-    this.osc.start();
+    this.oscillator = this.audioCtx.createOscillator();
+    this.oscillator.frequency.setValueAtTime(440, this.audioCtx.currentTime); // set osc frequency to 440 Hz
+    this.oscillator.connect(this.envelope.destination());
+    this.oscillator.start();
   }
 }
 
