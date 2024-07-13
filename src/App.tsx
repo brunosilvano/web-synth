fd7f4c4c72116737c5c0cc6103889b0e8b87e393
Bruno Silvano Costa <brunosilvanocosta@outlook.com>
2024-07-13 17:58:58 +0200

    Add sliders to control envelope times

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Synth from './Synth';
 import Keyboard from './Keyboard';
 
 function App({ synth }: { synth: Synth }) {
+  const [attack, setAttack] = useState(synth.envelope.attackTime);
+  const [decay, setDecay] = useState(synth.envelope.decayTime);
+
+  useEffect(() => {
+    synth.envelope.attackTime = attack;
+    synth.envelope.decayTime = decay;
+  }, [attack, decay, synth.envelope])
 
   const handleOnClick = (value: 'start' | 'stop') => {
     switch (value) {
@@ -21,10 +28,29 @@ function App({ synth }: { synth: Synth }) {
     synth.envelope.trigger();
   }
 
+  const handleParamChange = (stringValue: string, paramName: string) => {
+    const value = parseFloat(stringValue);
+
+    switch (paramName) {
+      case 'attack':
+        setAttack(value);
+        break;
+      case 'decay':
+        setDecay(value);
+        break;
+    }
+  }
+
   return (
     <div>
       <button onClick={() => handleOnClick('start')}>Start audio</button>
       <button onClick={() => handleOnClick('stop')}>Pause audio</button>
+      <div>
+        <label htmlFor="volume">Attack</label>
+        <input name="volume" type="range" min="0" max="5" step="0.01" value={attack} onChange={e => handleParamChange(e.target.value, 'attack')} />
+        <label htmlFor="volume">Decay</label>
+        <input name="volume" type="range" min="0" max="5" step="0.01" value={decay} onChange={e => handleParamChange(e.target.value, 'decay')} />
+      </div>
       <Keyboard onKeyDown={handleOnKeyDown} />
     </div>
   );
