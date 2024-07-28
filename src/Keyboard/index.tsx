30a607929cbc67fe7c5acedd2cbb5753e5473a20
Bruno Silvano Costa <brunosilvanocosta@outlook.com>
2024-07-28 09:49:31 +0200

    Listen to keyboard keys to play notes

--- src/Keyboard/index.tsx
+++ src/Keyboard/index.tsx
@@ -1,19 +1,41 @@
-import React from "react";
+import React, { useCallback, useEffect } from "react";
 import Key from "./Key";
 import { OnKeyDown } from "../App";
 
-const midiNotes = [
-  60, 62, 64, 65, 67, 69, 71, 72
+const keys = [
+  'a', 'w', 's', 'e', 'd', 'f', 't', 'g', 'y', 'h', 'u', 'j', 'k', 'o', 'l'
 ];
+
+const midiNotes = keys.map((key, index) => ({
+  key,
+  value: 60 +  index
+}));
 
 interface KeyboardProps {
   onKeyDown: OnKeyDown;
 }
 
 const Keyboard = ({ onKeyDown }: KeyboardProps) => {
+  const handleOnKeyDown = useCallback((ev: KeyboardEvent) => {
+    const key = ev.key;
+    if (ev.repeat) return;
+    if (keys.includes(key)) {
+      const note = midiNotes.find(midiNote => midiNote.key === ev.key)?.value ?? 60;
+      onKeyDown(note);
+    }
+  }, [onKeyDown]);
+
+  useEffect(() => {
+    document.removeEventListener('keydown', handleOnKeyDown);
+    document.addEventListener('keydown', handleOnKeyDown);
+    return () => {
+      document.removeEventListener('keydown', handleOnKeyDown);
+    }
+  }, [handleOnKeyDown]);
+
   return (
     <div style={{ display: 'flex' }}>
-      {midiNotes.map(midiNote => <Key midiNote={midiNote} onKeyDown={onKeyDown} />)}
+      {midiNotes.map(({ value })=> <Key midiNote={value} onKeyDown={onKeyDown} />)}
     </div>
   );
 }
