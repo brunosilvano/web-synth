e59b90adb1f2349c50b5464914e7cae1c06d27bb
Bruno Silvano Costa <16947700+brunosilvano@users.noreply.github.com>
2024-08-17 20:55:44 +0200

    Add support to control synth via computer keyboard (#7)
    * Listen to keyboard keys to play notes
    
    * Add support to octaves through z and x keys
    
    * Refactor key to MIDI note computation
    
    * Limit octaves to -2, 8 range
    
    * Add missing semicolons
    
    * Constraint MIDI notes to be between 0 and 127
    
    * Update README

--- src/Keyboard/index.tsx
+++ src/Keyboard/index.tsx
@@ -1,19 +1,56 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import Key from "./Key";
 import { OnKeyDown } from "../App";
 
-const midiNotes = [
-  60, 62, 64, 65, 67, 69, 71, 72
-];
+const keys = [
+  'a', 'w', 's', 'e', 'd', 'f', 't', 'g', 'y', 'h', 'u', 'j', 'k', 'o', 'l'
+] as const;
+
+const isInKeys = (x: string): x is typeof keys[number] => {
+  return keys.includes(x as typeof keys[number]);
+};
+
+const getMidiValue = (baseNote: number, octave: number) => {
+  return baseNote + (octave + 2) * 12;
+};
 
 interface KeyboardProps {
   onKeyDown: OnKeyDown;
 }
 
 const Keyboard = ({ onKeyDown }: KeyboardProps) => {
+  const [octave, setOctave] = useState(3);
+
+  const handleOnKeyDown = useCallback((ev: KeyboardEvent) => {
+    const key = ev.key;
+    if (ev.repeat) return;
+    if (isInKeys(key)) {
+      const note = getMidiValue(keys.indexOf(key), octave);
+      if (note < 0 && 127 < note) return;
+      onKeyDown(note);
+    } else if (key === 'x' && octave < 8) {
+      setOctave(currentOctave => ++currentOctave);
+    } else if (key === 'z' && octave > -2) {
+      setOctave(currentOctave => --currentOctave);
+    }
+  }, [octave, onKeyDown]);
+
+  useEffect(() => {
+    document.removeEventListener('keydown', handleOnKeyDown);
+    document.addEventListener('keydown', handleOnKeyDown);
+    return () => {
+      document.removeEventListener('keydown', handleOnKeyDown);
+    }
+  }, [handleOnKeyDown]);
+
   return (
     <div style={{ display: 'flex' }}>
-      {midiNotes.map(midiNote => <Key midiNote={midiNote} onKeyDown={onKeyDown} />)}
+      {keys.map((key, index) => {
+        const midiNote = getMidiValue(index, octave);
+        if (midiNote > 127) return null;
+        return <Key key={midiNote} midiNote={midiNote} onKeyDown={onKeyDown} />;
+      })
+      }
     </div>
   );
 }
